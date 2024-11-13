ccf017ef5143daecddd8b97cf3ecfb8e04c1875a
Wendy Steven <wndystvn13@mhs.mdp.ac.id>
2024-11-13 17:35:44 +0700

    Sign In Screen
    Create SignIn Screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:prj_wisatacandi_si51w/data/candi_data.dart';
 import 'package:prj_wisatacandi_si51w/screens/detail_screen.dart';
 import 'package:prj_wisatacandi_si51w/screens/profile_screen.dart';
+import 'package:prj_wisatacandi_si51w/screens/signin_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,10 +17,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        appBarTheme: const AppBarTheme(
+            iconTheme: IconThemeData(color: Colors.deepPurple),
+            titleTextStyle: TextStyle(
+                color: Colors.deepPurple,
+                fontSize: 20,
+                fontWeight: FontWeight.bold)),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)
+            .copyWith(
+                primary: Colors.deepPurple, surface: Colors.deepPurple[50]),
         useMaterial3: true,
       ),
-      home: ProfileScreen(),
+      home: SigninScreen(),
     );
   }
 }
